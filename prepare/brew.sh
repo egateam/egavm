ff31c5666880a2d316565543dd74508ee58b0efd
wang-q <wang-q@outlook.com>
2015-06-29 07:55:39 +0800

    Prompts for brew.sh

--- prepare/brew.sh
+++ prepare/brew.sh
@@ -1,19 +1,25 @@
 #!/usr/bin/env bash
 
-# linuxbrew
+echo " - Linuxbrew environment -"
+
+echo " + Clone latest linuxbrew"
 git clone https://github.com/Homebrew/linuxbrew.git ~/.linuxbrew
 
+echo " + Update .bashrc"
 echo '# Linuxbrew' >> ~/.bashrc
 echo 'export PATH="$HOME/.linuxbrew/bin:$PATH"' >> ~/.bashrc
 echo 'export MANPATH="$HOME/.linuxbrew/share/man:$MANPATH"' >> ~/.bashrc
 echo 'export INFOPATH="$HOME/.linuxbrew/share/info:$INFOPATH"' >> ~/.bashrc
 
 source ~/.bashrc
+
+echo " + Check brew with doctor"
 brew doctor
 
+echo " + Add tap science"
 brew tap homebrew/science
 
-# nodejs
+echo " + Install nodejs"
 brew install node
 npm --verbose -g install nodemon
 npm --verbose -g install bower
