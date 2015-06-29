223abf323c720ac2354e602827dec6abcf8acb3e
wang-q <wang-q@outlook.com>
2015-06-29 16:30:41 +0800

    Clone or pull

--- prepare/clone.sh
+++ prepare/clone.sh
@@ -1,31 +1,32 @@
 #!/usr/bin/env bash
 
-cd ~
-
-# make sure ~/bin in your $PATH
-mkdir -p bin
-
-# custom dependance
-git clone https://github.com/wang-q/faops.git
-cd faops && make && cp faops ~/bin/
+BASE_DIR=/home/vagrant
+cd $BASE_DIR
 
 # all parts of ega
-cd ~
+for OP in faops egaz egas alignDB withncbi
+do
+    if [ ! -d $BASE_DIR/$OP/.git ]
+    then
+        echo " + Clone $OP"
+        git clone https://github.com/wang-q/$OP.git $BASE_DIR/$OP
+    else
+        echo " + Pull $OP"
+        cd $BASE_DIR/$OP
+        git pull
+    fi
+done
 
-git clone https://github.com/wang-q/ega.git
-
-git clone https://github.com/wang-q/egaz.git
-
-git clone https://github.com/wang-q/egas.git
-
-git clone https://github.com/wang-q/alignDB.git
-
-git clone https://github.com/wang-q/withncbi.git
+# make sure ~/bin in your $PATH
+mkdir -p ~/bin
+cd $BASE_DIR/faops && make && cp faops ~/bin
 
 # node stuffs
-cd ~/ega
+echo " + Clone ega"
+sudo chown vagrant:vagrant $BASE_DIR/ega -R
+cd $BASE_DIR/ega
+git clone https://github.com/wang-q/ega.git tmp && mv tmp/.git . && rm -rf tmp && git reset --hard
 
 npm --verbose install
 bower --verbose install
 
-mkdir -p ~/ega/upload
