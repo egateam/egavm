c0ac0873cf02422cab38b07fed0ef50e5a6554f3
wang-q <wang-q@outlook.com>
2015-07-03 02:30:06 +0800

    Jim kent's userApps

---
+++ prepare/8-jksrc.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# kent userApps v308 generates much different result for maf* stuffs that looks very wrong. So don’t use it.
+cd /prepare/resource/
+unzip jksrc.zip # download in 2011
+cd kent/src
+
+sed -i 's/CC\=gcc/CC\=gcc \-Wno\-error\=unused\-but\-set\-variable /' inc/common.mk
+
+export MACHTYPE=x86_64
+export MYSQLLIBS="-L/home/vagrant/.linuxbrew/opt/mysql51/lib/mysql -lmysqlclient -lz"
+export MYSQLINC="/home/vagrant/.linuxbrew/opt/mysql51/include/mysql"
+make
+
