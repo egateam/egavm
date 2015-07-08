91f3a2ec755cb361a8e9305d0f2eaf3deb1c62cd
wang-q <wang-q@outlook.com>
2015-07-08 22:13:09 +0800

    Add JDK and gui tweaks

--- prepare/extra/1-apt.sh
+++ prepare/extra/1-apt.sh
@@ -11,11 +11,19 @@ sudo apt-get -y install gir1.2-glib-2.0 gir1.2-gtk-3.0 gir1.2-webkit-3.0
 echo " + Install gtk3 related tools"
 sudo apt-get -y install xvfb glade
 
+echo " + Install java (jdk)"
+sudo apt-get -y install openjdk-7-jdk
+
 echo " + Install nautilus plugins"
 sudo apt-get -y install nautilus-open-terminal nautilus-actions
 
-#echo " + Set Unity launcher items"
-#gsettings set com.canonical.Unity.Launcher favorites \
-#    "['application://nautilus.desktop', 'application://gnome-terminal.desktop', 'application://firefox.desktop', 'unity://running-apps', 'unity://expo-icon', 'unity://devices']"
+# These two lines should be execute inside a GUI
+# Set Unity launcher items
+#gsettings set com.canonical.Unity.Launcher favorites "['application://nautilus.desktop', 'application://gnome-terminal.desktop', 'application://firefox.desktop', 'unity://running-apps', 'unity://expo-icon', 'unity://devices']"
+
+# Disable unity lock screen
+# http://askubuntu.com/questions/177348/how-do-i-disable-the-screensaver-lock
+#gsettings set org.gnome.desktop.screensaver lock-enabled false
+#gsettings set org.gnome.desktop.session idle-delay 0 # (0 to disable)
 
 echo "Software installation complete!"
