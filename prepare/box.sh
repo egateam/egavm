2a3b949e6fd4f81864019e98c340ca890a004b11
wang-q <wang-q@outlook.com>
2015-07-08 08:06:16 +0800

    Use box-cutter's boxes

---
+++ prepare/box.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+vagrant box add box-cutter/ubuntu1404 --provider virtualbox
+
+vagrant box add box-cutter/ubuntu1404-desktop --provider virtualbox
+
+vagrant box add box-cutter/ubuntu1404-desktop --provider parallels
+
