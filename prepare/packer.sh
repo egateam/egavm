e3d8afe6564d2c57fa449c4366b18e0e64d2172f
wang-q <wang-q@outlook.com>
2015-07-09 09:42:19 +0800

    Misc changes

--- prepare/packer.sh
+++ prepare/packer.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-VG_DIR='/Users/wangq/Scripts/egavm/prepare'
+BASE_DIR='/Users/wangq/Scripts/egavm/prepare'
 
 echo "Don't execute this script."
 echo "Open it in a text editor then execute line by line."
@@ -18,18 +18,18 @@ exit 1
 brew cask install packer
 
 #
-cd $VG_DIR
+cd $BASE_DIR
 wget http://mirrors.ustc.edu.cn/ubuntu-releases/14.04.2/ubuntu-14.04.2-server-amd64.iso
 
 # Ubuntu template
 git clone https://github.com/boxcutter/ubuntu
-cd $VG_DIR/ubuntu
-echo "ISO_PATH := file://$VG_DIR" > Makefile.local
+cd $BASE_DIR/ubuntu
+echo "ISO_PATH := file://$BASE_DIR" > Makefile.local
 echo "CM := nocm" >> Makefile.local
 #rm box/virtualbox/ubuntu1404-nocm-*.box
 make virtualbox/ubuntu1404
 
-### VirtualBox
+### Parallels
 
 # Install Parallels manually
 # http://www.parallels.com/products/desktop/
@@ -37,6 +37,6 @@ make virtualbox/ubuntu1404
 # install Parallels provider for vagrant
 vagrant plugin install vagrant-parallels
 
-cd $VG_DIR/ubuntu
+cd $BASE_DIR/ubuntu
 #rm box/parallels/ubuntu1404-nocm-*.box
 make parallels/ubuntu1404
