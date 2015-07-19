1b237e261fad6324ddce90e7f20bfd1bdfa6fd77
wang-q <wang-q@outlook.com>
2015-07-20 01:46:51 +0800

    Misc changes

--- prepare/4-cpanm.sh
+++ prepare/4-cpanm.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
+CPAN_MIRROR=http://mirrors.ustc.edu.cn/CPAN/
+#CPAN_MIRROR=http://www.cpan.org/
+
 # very basic
-cpanm --mirror-only --mirror http://mirrors.ustc.edu.cn/CPAN/ Carp Cwd Config::Tiny DB_File File::Find::Rule JSON JSON::XS Text::CSV Text::CSV_XS YAML
-cpanm --mirror-only --mirror http://mirrors.ustc.edu.cn/CPAN/ DBI MCE Moose Template
-cpanm --mirror-only --mirror http://mirrors.ustc.edu.cn/CPAN/ --notest App::Ack EV AnyEvent POE WWW::Mechanize
+cpanm --mirror-only --mirror $CPAN_MIRROR Config::Tiny DB_File File::Find::Rule JSON JSON::XS Text::CSV Text::CSV_XS YAML
+cpanm --mirror-only --mirror $CPAN_MIRROR --notest App::Ack EV AnyEvent DBI MCE Moose POE Template WWW::Mechanize
 
 # linuxbrew's pkg-config (node's dependency) overide system pkg-config, and don't inherit system PKG_CONFIG_PATH
 # See also https://github.com/Homebrew/linuxbrew/issues/47
@@ -15,29 +17,29 @@ cpanm --mirror-only --mirror http://mirrors.ustc.edu.cn/CPAN/ --notest App::Ack 
 #
 # In my current preference, I tend to use apt to install various development packages.
 #export PKG_CONFIG_PATH=/usr/share/pkgconfig/:/usr/lib/x86_64-linux-gnu/pkgconfig/:$PKG_CONFIG_PATH
-cpanm --mirror-only --mirror http://mirrors.ustc.edu.cn/CPAN/ GD SVG GD::SVG
+cpanm --mirror-only --mirror $CPAN_MIRROR GD SVG GD::SVG
 
 # bioperl
-cpanm --mirror-only --mirror http://mirrors.ustc.edu.cn/CPAN/ Data::Stag Test::Most URI::Escape Algorithm::Munkres Array::Compare Clone Error File::Sort Graph IO::Scalar List::MoreUtils Set::Scalar Sort::Naturally
-cpanm --mirror-only --mirror http://mirrors.ustc.edu.cn/CPAN/ HTML::Entities HTML::HeadParser HTML::TableExtract HTTP::Request::Common LWP::UserAgent PostScript::TextBlock
-cpanm --mirror-only --mirror http://mirrors.ustc.edu.cn/CPAN/ XML::DOM XML::DOM::XPath XML::LibXML XML::SAX::Writer XML::Simple XML::Twig XML::Writer GraphViz SVG SVG::Graph
-cpanm --mirror-only --mirror http://mirrors.ustc.edu.cn/CPAN/ --notest Convert::Binary::C YAML::Syck
+cpanm --mirror-only --mirror $CPAN_MIRROR Data::Stag Test::Most URI::Escape Algorithm::Munkres Array::Compare Clone Error File::Sort Graph IO::Scalar List::MoreUtils Set::Scalar Sort::Naturally
+cpanm --mirror-only --mirror $CPAN_MIRROR HTML::Entities HTML::HeadParser HTML::TableExtract HTTP::Request::Common LWP::UserAgent PostScript::TextBlock
+cpanm --mirror-only --mirror $CPAN_MIRROR XML::DOM XML::DOM::XPath XML::LibXML XML::SAX::Writer XML::Simple XML::Twig XML::Writer GraphViz SVG SVG::Graph
+cpanm --mirror-only --mirror $CPAN_MIRROR --notest Convert::Binary::C YAML::Syck
 
-cpanm --mirror-only --mirror http://mirrors.ustc.edu.cn/CPAN/ --notest Bio::Perl # BioPerl
-cpanm --mirror-only --mirror http://mirrors.ustc.edu.cn/CPAN/ Bio::ASN1::EntrezGene Bio::DB::EUtilities Bio::Graphics
-cpanm --mirror-only --mirror http://mirrors.ustc.edu.cn/CPAN/ --notest Bio::Tools::Run::Alignment::Clustalw # BioPerl-Run
+cpanm --mirror-only --mirror $CPAN_MIRROR --notest Bio::Perl # BioPerl
+cpanm --mirror-only --mirror $CPAN_MIRROR Bio::ASN1::EntrezGene Bio::DB::EUtilities Bio::Graphics
+cpanm --mirror-only --mirror $CPAN_MIRROR --notest Bio::Tools::Run::Alignment::Clustalw # BioPerl-Run
 
 # circos
-cpanm --mirror-only --mirror http://mirrors.ustc.edu.cn/CPAN/ Config::General Data::Dumper Digest::MD5 Font::TTF::Font Math::Bezier Math::BigFloat Math::Round Math::VecStat
-cpanm --mirror-only --mirror http://mirrors.ustc.edu.cn/CPAN/ Params::Validate Readonly Regexp::Common Set::IntSpan Statistics::Basic Text::Balanced Text::Format Time::HiRes
+cpanm --mirror-only --mirror $CPAN_MIRROR Config::General Data::Dumper Digest::MD5 Font::TTF::Font Math::Bezier Math::BigFloat Math::Round Math::VecStat
+cpanm --mirror-only --mirror $CPAN_MIRROR Params::Validate Readonly Regexp::Common Set::IntSpan Statistics::Basic Text::Balanced Text::Format Time::HiRes
 
 # Bio::Phylo
-cpanm --mirror-only --mirror http://mirrors.ustc.edu.cn/CPAN/ XML::XML2JSON PDF::API2 Math::CDF Math::Random
+cpanm --mirror-only --mirror $CPAN_MIRROR XML::XML2JSON PDF::API2 Math::CDF Math::Random
 cpanm --notest Bio::Phylo
 
 # others
-cpanm --mirror-only --mirror http://mirrors.ustc.edu.cn/CPAN/ DateTime::Format::Natural File::HomeDir DBD::CSV Path::Class File::Listing File::Remove File::Rename File::Copy::Recursive String::Compare
-cpanm --mirror-only --mirror http://mirrors.ustc.edu.cn/CPAN/ --notest LWP::Online LWP::Protocol::https
+cpanm --mirror-only --mirror $CPAN_MIRROR DateTime::Format::Natural File::HomeDir DBD::CSV Path::Class File::Listing File::Remove File::Rename File::Copy::Recursive String::Compare
+cpanm --mirror-only --mirror $CPAN_MIRROR --notest LWP::Online LWP::Protocol::https
 
 # AlignDB::*
 cpanm --mirror-only --mirror https://stratopan.com/wangq/alignDB/master AlignDB::IntSpan AlignDB::Util AlignDB::Stopwatch AlignDB::Codon
--- prepare/extra/4-cpanm.sh
+++ prepare/extra/4-cpanm.sh
@@ -2,36 +2,38 @@
 
 # This is a minimal set of modules, which means all intermediate modules are removed.
 
+CPAN_MIRROR=http://mirrors.ustc.edu.cn/CPAN/
+
 # Test::*
-cpanm --mirror-only --mirror http://mirrors.ustc.edu.cn/CPAN/ Test::Class Test::Roo Test::Taint Test::Without::Module
+cpanm --mirror-only --mirror $CPAN_MIRROR Test::Class Test::Roo Test::Taint Test::Without::Module
 
 # Moose and Moo
-cpanm --mirror-only --mirror http://mirrors.ustc.edu.cn/CPAN/ MooX::Options MooseX::Getopt MooseX::Role::WithOverloading MooseX::Storage MooseX::Types::Path::Tiny
+cpanm --mirror-only --mirror $CPAN_MIRROR MooX::Options MooseX::Getopt MooseX::Role::WithOverloading MooseX::Storage MooseX::Types::Path::Tiny
 
 # CPAN
-cpanm --mirror-only --mirror http://mirrors.ustc.edu.cn/CPAN/ App::pmuninstall CPAN::Mini CPANDB Dist::Zilla::Plugin::Git Dist::Zilla::Plugin::PerlTidy Dist::Zilla::Plugin::PodWeaver Module::Path Pod::POM::Web
+cpanm --mirror-only --mirror $CPAN_MIRROR App::pmuninstall CPAN::Mini CPANDB Dist::Zilla::Plugin::Git Dist::Zilla::Plugin::PerlTidy Dist::Zilla::Plugin::PodWeaver Module::Path Pod::POM::Web
 
 # Gtk3 stuffs
-cpanm --mirror-only --mirror http://mirrors.ustc.edu.cn/CPAN/ Glib Cairo Cairo::GObject Glib::Object::Introspection Gtk3
+cpanm --mirror-only --mirror $CPAN_MIRROR Glib Cairo Cairo::GObject Glib::Object::Introspection Gtk3
 
 # Database and WWW related
-cpanm --mirror-only --mirror http://mirrors.ustc.edu.cn/CPAN/ DBIx::Class::Helpers DBIx::XHTML_Table DBM::Deep MongoDB Mojolicious
+cpanm --mirror-only --mirror $CPAN_MIRROR DBIx::Class::Helpers DBIx::XHTML_Table DBM::Deep MongoDB Mojolicious
 
 # Develop
-cpanm --mirror-only --mirror http://mirrors.ustc.edu.cn/CPAN/ Parallel::ForkManager Proc::Background Devel::CheckLib Import::Into
+cpanm --mirror-only --mirror $CPAN_MIRROR Parallel::ForkManager Proc::Background Devel::CheckLib Import::Into
 
 # text, rtf and xlsx
-cpanm --mirror-only --mirror http://mirrors.ustc.edu.cn/CPAN/ Roman Text::Table Time::Duration Excel::Writer::XLSX Spreadsheet::ParseExcel Spreadsheet::WriteExcel RTF::Writer
+cpanm --mirror-only --mirror $CPAN_MIRROR Roman Text::Table Time::Duration Excel::Writer::XLSX Spreadsheet::ParseExcel Spreadsheet::WriteExcel RTF::Writer
 
 # Math & Stat
-cpanm --mirror-only --mirror http://mirrors.ustc.edu.cn/CPAN/ Math::Random::MT::Auto PDL Statistics::Lite Statistics::TTest Set::IntSpan::Fast Set::Light
+cpanm --mirror-only --mirror $CPAN_MIRROR Math::Random::MT::Auto PDL Statistics::Lite Statistics::TTest Set::IntSpan::Fast Set::Light
 
 # others
-cpanm --mirror-only --mirror http://mirrors.ustc.edu.cn/CPAN/ Chart::Math::Axis List::Flatten PPI::XS
+cpanm --mirror-only --mirror $CPAN_MIRROR Chart::Math::Axis List::Flatten PPI::XS
 
-cpanm --mirror-only --mirror http://mirrors.ustc.edu.cn/CPAN/ --notest MooseX::AttributeHelpers
+cpanm --mirror-only --mirror $CPAN_MIRROR --notest MooseX::AttributeHelpers
 
 # with external lib
-cpanm --mirror-only --mirror http://mirrors.ustc.edu.cn/CPAN/ --notest Math::GSL Statistics::R
+cpanm --mirror-only --mirror $CPAN_MIRROR --notest Math::GSL Statistics::R
 
 # DBD::mysql installed after brew mysql51
