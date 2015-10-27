b85d7deca804fcb611df07f21591de5910007108
wang-q <wang-q@outlook.com>
2015-10-27 13:05:24 +0800

    Fix typo

--- prepare/6-download.sh
+++ prepare/6-download.sh
@@ -53,26 +53,26 @@ echo "==> kent bin"
 cd $HOME/bin/
 if [[ `uname` == 'Darwin' ]];
 then
-    JBBIN_TAR_GZ=/prepare/resource/jkbin-darwin-2011.tar.gz
+    JKBIN_TAR_GZ=/prepare/resource/jkbin-darwin-2011.tar.gz
 else
-    JBBIN_TAR_GZ=/prepare/resource/jkbin-ubuntu-1404-2011.tar.gz
+    JKBIN_TAR_GZ=/prepare/resource/jkbin-ubuntu-1404-2011.tar.gz
 fi
-echo "==> untar from ${JBBIN_TAR_GZ}"
-tar xvfz ${JBBIN_TAR_GZ} x86_64/axtChain
-tar xvfz ${JBBIN_TAR_GZ} x86_64/axtSort
-tar xvfz ${JBBIN_TAR_GZ} x86_64/axtToMaf
-tar xvfz ${JBBIN_TAR_GZ} x86_64/chainAntiRepeat
-tar xvfz ${JBBIN_TAR_GZ} x86_64/chainMergeSort
-tar xvfz ${JBBIN_TAR_GZ} x86_64/chainNet
-tar xvfz ${JBBIN_TAR_GZ} x86_64/chainPreNet
-tar xvfz ${JBBIN_TAR_GZ} x86_64/chainSplit
-tar xvfz ${JBBIN_TAR_GZ} x86_64/chainStitchId
-tar xvfz ${JBBIN_TAR_GZ} x86_64/faSize
-tar xvfz ${JBBIN_TAR_GZ} x86_64/faToTwoBit
-tar xvfz ${JBBIN_TAR_GZ} x86_64/lavToPsl
-tar xvfz ${JBBIN_TAR_GZ} x86_64/mafSpeciesList
-tar xvfz ${JBBIN_TAR_GZ} x86_64/netChainSubset
-tar xvfz ${JBBIN_TAR_GZ} x86_64/netFilter
-tar xvfz ${JBBIN_TAR_GZ} x86_64/netSplit
-tar xvfz ${JBBIN_TAR_GZ} x86_64/netSyntenic
-tar xvfz ${JBBIN_TAR_GZ} x86_64/netToAxt
+echo "==> untar from ${JKBIN_TAR_GZ}"
+tar xvfz ${JKBIN_TAR_GZ} x86_64/axtChain
+tar xvfz ${JKBIN_TAR_GZ} x86_64/axtSort
+tar xvfz ${JKBIN_TAR_GZ} x86_64/axtToMaf
+tar xvfz ${JKBIN_TAR_GZ} x86_64/chainAntiRepeat
+tar xvfz ${JKBIN_TAR_GZ} x86_64/chainMergeSort
+tar xvfz ${JKBIN_TAR_GZ} x86_64/chainNet
+tar xvfz ${JKBIN_TAR_GZ} x86_64/chainPreNet
+tar xvfz ${JKBIN_TAR_GZ} x86_64/chainSplit
+tar xvfz ${JKBIN_TAR_GZ} x86_64/chainStitchId
+tar xvfz ${JKBIN_TAR_GZ} x86_64/faSize
+tar xvfz ${JKBIN_TAR_GZ} x86_64/faToTwoBit
+tar xvfz ${JKBIN_TAR_GZ} x86_64/lavToPsl
+tar xvfz ${JKBIN_TAR_GZ} x86_64/mafSpeciesList
+tar xvfz ${JKBIN_TAR_GZ} x86_64/netChainSubset
+tar xvfz ${JKBIN_TAR_GZ} x86_64/netFilter
+tar xvfz ${JKBIN_TAR_GZ} x86_64/netSplit
+tar xvfz ${JKBIN_TAR_GZ} x86_64/netSyntenic
+tar xvfz ${JKBIN_TAR_GZ} x86_64/netToAxt
