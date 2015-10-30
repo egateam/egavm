d60fb0dcf3b82ae9e99d850ec66fd7fcf64b70de
wang-q <wang-q@outlook.com>
2015-10-30 10:49:01 +0800

    File size limits in VM

--- prepare/8-node.sh
+++ prepare/8-node.sh
@@ -22,6 +22,6 @@ cp $BASE_DIR/ega/settings.js.example $BASE_DIR/ega/settings.js
 if [[ `whoami` == 'vagrant' ]];
 then
     echo "==> Remove file size limits in VM"
-    perl -npi -e 's{file_size_limit\:.+}{file_size_limit\: 30 \* 1024 \* 1024 \* 1024 \/\/ 30 GB}' $BASE_DIR/ega/settings.js
-    perl -npi -e 's{data\-validation\-max\-size\=\"30M\"}{data\-validation\-max\-size="30G"}' $BASE_DIR/ega/views/upload.jade
+    perl -npi -e 's{file_size_limit\:.+}{file_size_limit\: 999 \* 1024 \* 1024 \/\/ 999 MB}' $BASE_DIR/ega/settings.js
+    perl -npi -e 's{data\-validation\-max\-size\=\"30M\"}{data\-validation\-max\-size="999M"}' $BASE_DIR/ega/views/upload.jade
 fi
