2e604877ecbca4de5fe8a685507e511f4feaa6d4
wang-q <wang-q@outlook.com>
2015-11-03 02:44:07 +0800

    Download files required by linuxbrew

---
+++ prepare/resource/brew-pre-download.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+wget -N http://mirrors.ustc.edu.cn/CPAN/authors/id/R/RJ/RJBS/perl-5.18.4.tar.bz2
+wget -N http://mirrors.ustc.edu.cn/CPAN/authors/id/R/RJ/RJBS/perl-5.18.4.tar.gz
+
+wget -N ftp://ftp.ncbi.nlm.nih.gov/blast/executables/blast+/2.2.31/ncbi-blast-2.2.31+-src.tar.gz
+wget -N ftp://ftp.ncbi.nlm.nih.gov/blast/executables/rmblast/LATEST/ncbi-rmblastn-2.2.28-x64-linux.tar.gz
+wget -N http://www.bzip.org/1.0.6/bzip2-1.0.6.tar.gz
+wget -N http://www.clustal.org/download/2.1/clustalw-2.1.tar.gz
+wget -N http://selab.janelia.org/software/hmmer3/3.1b2/hmmer-3.1b2.tar.gz
+wget -N http://www.ijg.org/files/jpegsrc.v8d.tar.gz
+wget -N http://www.bx.psu.edu/miller_lab/dist/lastz-1.02.00.tar.gz
+wget -N http://mafft.cbrc.jp/alignment/software/mafft-7.221-with-extensions-src.tgz
+wget -N http://pkgconfig.freedesktop.org/releases/pkg-config-0.28.tar.gz
+wget -N https://ftp.csx.cam.ac.uk/pub/software/programming/pcre/pcre-8.37.tar.bz2
+wget -N http://221.204.176.53/openssl-1.0.2c.tar.gz
+wget -N http://mirror.hust.edu.cn/gnu/readline/readline-6.3.tar.gz
+wget -N https://github.com/stamatak/standard-RAxML/archive/v8.1.15.tar.gz
+wget -N http://www.repeatmasker.org/RepeatMasker-open-4-0-5.tar.gz
+wget -N http://cegg.unige.ch/pub/newick-utils-1.6.tar.gz
+wget -N http://mysql.mirrors.pair.com/Downloads/MySQL-5.1/mysql-5.1.73.tar.gz
+wget -N https://fossies.org/linux/misc/xz-5.2.1.tar.gz
+
+wget -N https://nodejs.org/dist/v0.12.7/node-v0.12.7.tar.gz
+wget -N https://registry.npmjs.org/npm/-/npm-2.14.4.tgz
