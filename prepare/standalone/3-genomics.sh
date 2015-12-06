fb498de84bd56bc43f44d47bac8e90a78d5f94bc
wang-q <wang-q@outlook.com>
2015-12-07 06:54:32 +0800

    Gatk and picard

--- prepare/standalone/3-genomics.sh
+++ prepare/standalone/3-genomics.sh
@@ -30,3 +30,40 @@ tar xvfz /prepare/resource/v2.0.6.tar.gz
 cd $HOME/share/trinityrnaseq-*
 make
 make plugins
+
+echo "==> gatk"
+# brew install maven
+# sudo apt-get install maven
+
+if [ ! -e /prepare/resource/gatk-3.5.tar.gz ];
+then
+    cd /prepare/resource/
+    wget -N https://github.com/broadgsa/gatk-protected/archive/3.5.tar.gz
+
+    cd $HOME/share/
+    tar xvfz /prepare/resource/3.5.tar.gz
+
+    cd $HOME/share/gatk-protected-*
+    # Compile the GATK but not Queue
+    mvn verify -P\!queue
+    mv target/executable $HOME/share/gatk
+
+    cd $HOME/share
+    tar cvfz gatk-3.5.tar.gz gatk
+    mv gatk-3.5.tar.gz /prepare/resource/
+else
+    cd $HOME/share/
+    tar xvfz /prepare/resource/gatk-3.5.tar.gz
+fi
+
+cd $HOME/share/
+java -jar java -jar gatk/GenomeAnalysisTK.jar --help
+
+echo "==> picard"
+cd /prepare/resource/
+wget -N https://github.com/broadinstitute/picard/releases/download/1.128/picard-tools-1.128.zip
+
+cd $HOME/share/
+unzip /prepare/resource/picard-tools-1.128.zip
+
+java -jar picard-tools-1.128/picard.jar -h
