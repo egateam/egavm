aea9ea2624ea2327607590458a063a1225f82ce3
wang-q <wang-q@outlook.com>
2015-12-28 04:56:05 +0800

    Add sparsemem

--- prepare/7-brew.sh
+++ prepare/7-brew.sh
@@ -69,7 +69,7 @@ cp $HOME/.linuxbrew/lib/perl5/site_perl/Vcf.pm      `perl -e 'print grep {/site_
 cp $HOME/.linuxbrew/lib/perl5/site_perl/VcfStats.pm `perl -e 'print grep {/site_perl/} grep {!/x86_64/} @INC'`
 
 echo "==> Install wang-q/tap"
-brew install faops multiz
+brew install faops multiz sparsemem
 
 echo "==> Config repeatmasker"
 cd `brew --prefix`/Cellar/repeatmasker/4.0.5/libexec
