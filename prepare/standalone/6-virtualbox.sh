2c6d11cd07202a5f7c31c29e98fe248ee3be0964
wang-q <wang-q@outlook.com>
2016-01-12 13:31:03 +0800

    packer and vagrant

--- prepare/standalone/6-virtualbox.sh
+++ prepare/standalone/6-virtualbox.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
+echo "==> dkms"
 sudo apt-get -y update
 sudo apt-get -y install dkms
 
+echo "==> virtualbox"
 sudo tee -a /etc/apt/sources.list  <<EOF
 
 # virtualbox
@@ -14,3 +16,12 @@ wget -q https://www.virtualbox.org/download/oracle_vbox.asc -O- | sudo apt-key a
 
 sudo apt-get -y update
 sudo apt-get -y install virtualbox-5.0
+
+echo "==> packer"
+mkdir -p ~/bin
+wget -N -P /prepare/resource https://releases.hashicorp.com/packer/0.8.6/packer_0.8.6_linux_amd64.zip
+unzip /prepare/resource/packer_*_linux_amd64.zip -d ~/bin
+
+echo "==> vagrant"
+wget -N -P /prepare/resource https://releases.hashicorp.com/vagrant/1.8.1/vagrant_1.8.1_x86_64.deb
+sudo dpkg -i /prepare/resource/vagrant_1.8.1_x86_64.deb
