9f08cb1104ba953aabcc8542f20e2015888741c2
wang-q <wang-q@outlook.com>
2016-01-19 09:54:37 +0800

    Misc changes

--- prepare/extra/4-cpanm.sh
+++ prepare/extra/4-cpanm.sh
@@ -10,33 +10,23 @@ NO_TEST=--notest
 cpanm --mirror-only --mirror $CPAN_MIRROR $NO_TEST Test::Class Test::Roo Test::Taint Test::Without::Module
 
 # Moose and Moo
-cpanm --mirror-only --mirror $CPAN_MIRROR $NO_TEST MooX::Options MooseX::Getopt MooseX::Role::WithOverloading MooseX::Storage MooseX::Types::Path::Tiny
+cpanm --mirror-only --mirror $CPAN_MIRROR --notest MooX::Options MooseX::Storage MooseX::AttributeHelpers
 
-# CPAN
+# Develop
 cpanm --mirror-only --mirror $CPAN_MIRROR $NO_TEST App::pmuninstall CPAN::Mini CPANDB Dist::Milla Module::Path Pod::POM::Web Search::Indexer
+cpanm --mirror-only --mirror $CPAN_MIRROR $NO_TEST Parallel::ForkManager Proc::Background Devel::CheckLib Import::Into PPI::XS
 
 # Gtk3 stuffs
 cpanm --mirror-only --mirror $CPAN_MIRROR $NO_TEST Glib Cairo Cairo::GObject Glib::Object::Introspection Gtk3
 
-# Database and WWW related
+# Database and WWW
 cpanm --mirror-only --mirror $CPAN_MIRROR $NO_TEST DBIx::Class::Helpers DBIx::XHTML_Table DBM::Deep MongoDB Mojolicious IO::All IO::All::LWP
 
-# Develop
-cpanm --mirror-only --mirror $CPAN_MIRROR $NO_TEST Parallel::ForkManager Proc::Background Devel::CheckLib Import::Into
-
 # text, rtf and xlsx
-cpanm --mirror-only --mirror $CPAN_MIRROR $NO_TEST Roman Text::Table Time::Duration Excel::Writer::XLSX Spreadsheet::ParseExcel Spreadsheet::WriteExcel RTF::Writer
+cpanm --mirror-only --mirror $CPAN_MIRROR $NO_TEST Roman Text::Table Time::Duration Excel::Writer::XLSX Spreadsheet::ParseExcel Spreadsheet::WriteExcel RTF::Writer Chart::Math::Axis
 
-# Math & Stat
-cpanm --mirror-only --mirror $CPAN_MIRROR $NO_TEST Math::Random::MT::Auto PDL Statistics::Lite Statistics::TTest Set::IntSpan::Fast Set::Light
-
-# others
-cpanm --mirror-only --mirror $CPAN_MIRROR $NO_TEST Chart::Math::Axis PPI::XS
-
-cpanm --mirror-only --mirror $CPAN_MIRROR --notest MooseX::AttributeHelpers
-
-# with external lib
-cpanm --mirror-only --mirror $CPAN_MIRROR --notest Math::GSL Statistics::R
+# Math and Stat
+cpanm --mirror-only --mirror $CPAN_MIRROR --notest Math::Random::MT::Auto PDL Statistics::Lite Statistics::TTest Set::Light Math::GSL Statistics::R
 
 # AlignDB::*
 cpanm --mirror-only --mirror https://stratopan.com/wangq/ega/master $NO_TEST AlignDB::DeltaG AlignDB::GC AlignDB::SQL AlignDB::ToXLSX
