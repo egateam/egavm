08aae415631c8a85b470acaf38d284c17ca92423
wang-q <wang-q@outlook.com>
2016-04-12 13:56:05 +0800

    Add genometools for gt-merge

--- prepare/standalone/3-genomics.sh
+++ prepare/standalone/3-genomics.sh
@@ -13,7 +13,7 @@ brew install cmake
 brew install samtools bamtools htslib bowtie bowtie2
 brew install tophat cufflinks
 brew install sratoolkit fastqc sickle
-brew install bedtools bedops
+brew install bedtools bedops genometools
 
 echo "==> custom tap"
 brew tap wang-q/tap
