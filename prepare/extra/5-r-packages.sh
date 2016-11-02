a27b7af2913944d97e5a5c392805eec260184947
wang-q <wang-q@outlook.com>
2016-11-02 17:24:54 +0800

    Remove stringr

--- prepare/extra/5-r-packages.sh
+++ prepare/extra/5-r-packages.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 
-Rscript -e 'install.packages("getopt", repos="http://mirrors.ustc.edu.cn/CRAN/")'
-Rscript -e 'install.packages("stringr", repos="http://mirrors.ustc.edu.cn/CRAN/")'
-Rscript -e 'install.packages("ape", repos="http://mirrors.ustc.edu.cn/CRAN/")'
+Rscript -e 'install.packages("getopt", repos="https://mirrors.tuna.tsinghua.edu.cn/CRAN")'
+Rscript -e 'install.packages("ape", repos="https://mirrors.tuna.tsinghua.edu.cn/CRAN")'
