47f256dee5404cb5ed50a092002a9c384cd75601
wang-q <wang-q@outlook.com>
2017-09-07 21:44:06 +0800

    Update downloading list

--- resource/download.sh
+++ resource/download.sh
@@ -1,32 +1,31 @@
 #!/usr/bin/env bash
 
-DOWNLOADDIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+BASE_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
 # ubuntu
-wget -N -P ${DOWNLOADDIR} http://mirrors.ustc.edu.cn/ubuntu-releases/14.04.5/ubuntu-14.04.5-server-amd64.iso
-wget -N -P ${DOWNLOADDIR} http://mirrors.ustc.edu.cn/ubuntu-releases/14.04.5/ubuntu-14.04.5-desktop-amd64.iso
+wget -N -P ${BASE_DIR} http://mirrors.ustc.edu.cn/ubuntu-releases/14.04.5/ubuntu-14.04.5-server-amd64.iso
+wget -N -P ${BASE_DIR} http://mirrors.ustc.edu.cn/ubuntu-releases/14.04.5/ubuntu-14.04.5-desktop-amd64.iso
 
 # virtualbox
-wget -N -P ${DOWNLOADDIR} http://download.virtualbox.org/virtualbox/5.1.12/VirtualBox-5.1.12-112440-OSX.dmg
-wget -N -P ${DOWNLOADDIR} http://download.virtualbox.org/virtualbox/5.1.12/VirtualBox-5.1.12-112440-Win.exe
-wget -N -P ${DOWNLOADDIR} http://download.virtualbox.org/virtualbox/5.1.12/Oracle_VM_VirtualBox_Extension_Pack-5.1.12-112440.vbox-extpack
+wget -N -P ${BASE_DIR} http://download.virtualbox.org/virtualbox/5.1.26/VirtualBox-5.1.26-117224-OSX.dmg
+wget -N -P ${BASE_DIR} http://download.virtualbox.org/virtualbox/5.1.26/VirtualBox-5.1.26-117224-Win.exe
+
+wget -N -P ${BASE_DIR} http://download.virtualbox.org/virtualbox/5.1.26/Oracle_VM_VirtualBox_Extension_Pack-5.1.26-117224.vbox-extpack
 
 # virtualbox additions
-wget -N -P ${DOWNLOADDIR} http://download.virtualbox.org/virtualbox/5.1.12/VBoxGuestAdditions_5.1.12.iso
+wget -N -P ${BASE_DIR} http://download.virtualbox.org/virtualbox/5.1.26/VBoxGuestAdditions_5.1.26.iso
 
 # perl
-wget -N -P ${DOWNLOADDIR} http://mirrors.ustc.edu.cn/CPAN/authors/id/R/RJ/RJBS/perl-5.18.4.tar.bz2
-wget -N -P ${DOWNLOADDIR} http://mirrors.ustc.edu.cn/CPAN/authors/id/R/RJ/RJBS/perl-5.18.4.tar.gz
+wget -N -P ${BASE_DIR} http://mirrors.ustc.edu.cn/CPAN/authors/id/R/RJ/RJBS/perl-5.18.4.tar.bz2
+wget -N -P ${BASE_DIR} http://mirrors.ustc.edu.cn/CPAN/authors/id/R/RJ/RJBS/perl-5.18.4.tar.gz
 
 # 7-brew.sh bioinformatics tools
-wget -N -P ${DOWNLOADDIR} ftp://ftp.ncbi.nlm.nih.gov/blast/executables/blast+/2.5.0/ncbi-blast-2.5.0+-src.tar.gz
-wget -N -P ${DOWNLOADDIR} ftp://ftp.ncbi.nlm.nih.gov/blast/executables/rmblast/LATEST/ncbi-rmblastn-2.2.28-x64-linux.tar.gz
-wget -N -P ${DOWNLOADDIR} mirrors.ustc.edu.cn/CRAN/src/base/R-3/R-3.3.2.tar.gz
+wget -N -P ${BASE_DIR} ftp://ftp.ncbi.nlm.nih.gov/blast/executables/rmblast/LATEST/ncbi-rmblastn-2.2.28-x64-linux.tar.gz
 
-if [ ! -e ${DOWNLOADDIR}/RepeatMasker-open-4-0-5.tar.gz ];
+if [ ! -e ${BASE_DIR}/RepeatMasker-open-4-0-5.tar.gz ];
 then
-    wget -N -P ${DOWNLOADDIR} http://www.repeatmasker.org/RepeatMasker-open-4-0-5.tar.gz
+    wget -N -P ${BASE_DIR} http://www.repeatmasker.org/RepeatMasker-open-4-0-5.tar.gz
 fi
 
 # extra/7-mysql.sh
-wget -N -P ${DOWNLOADDIR} http://mysql.mirrors.pair.com/Downloads/MySQL-5.1/mysql-5.1.73.tar.gz
+wget -N -P ${BASE_DIR} http://mysql.mirrors.pair.com/Downloads/MySQL-5.1/mysql-5.1.73.tar.gz
