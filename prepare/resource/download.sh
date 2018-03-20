e6a571d9f4725f095945aa06a9fdb6292c57325b
wang-q <wang-q@outlook.com>
2018-03-20 15:48:00 +0800

    Misc changes

--- prepare/resource/download.sh
+++ prepare/resource/download.sh
@@ -3,10 +3,9 @@
 BASE_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
 # ubuntu
-if [ ! -e ${BASE_DIR}/ubuntu-14.04.5-*.iso ];
-then
+if [ ! -e ${BASE_DIR}/ubuntu-14.04.5-server-amd64.iso ]; then
     wget -N -P ${BASE_DIR} http://mirrors.ustc.edu.cn/ubuntu-releases/14.04.5/ubuntu-14.04.5-server-amd64.iso
-#    wget -N -P ${BASE_DIR} http://mirrors.ustc.edu.cn/ubuntu-releases/14.04.5/ubuntu-14.04.5-desktop-amd64.iso
+    wget -N -P ${BASE_DIR} http://mirrors.ustc.edu.cn/ubuntu-releases/14.04.5/ubuntu-14.04.5-desktop-amd64.iso
 fi
 
 # virtualbox
@@ -18,15 +17,20 @@ wget -N -P ${BASE_DIR} https://download.virtualbox.org/virtualbox/5.2.8/Oracle_V
 # virtualbox additions
 wget -N -P ${BASE_DIR} http://download.virtualbox.org/virtualbox/5.2.8/VBoxGuestAdditions_5.2.8.iso
 
-# perl
-wget -N -P ${BASE_DIR} http://mirrors.ustc.edu.cn/CPAN/authors/id/R/RJ/RJBS/perl-5.18.4.tar.bz2
+# portable ruby
+wget -N -P ${BASE_DIR} https://homebrew.bintray.com/bottles-portable-ruby/portable-ruby-2.3.3.x86_64_linux.bottle.1.tar.gz
+
+# perl source
 wget -N -P ${BASE_DIR} http://mirrors.ustc.edu.cn/CPAN/authors/id/R/RJ/RJBS/perl-5.18.4.tar.gz
+
+# large bottles
+wget -N -P ${BASE_DIR} https://linuxbrew.bintray.com/bottles/jdk-9.0.1+11.x86_64_linux.bottle.tar.gz
+wget -N -P ${BASE_DIR} https://linuxbrew.bintray.com/bottles/blast-2.7.1.x86_64_linux.bottle.tar.gz
 
 # 7-brew.sh bioinformatics tools
 wget -N -P ${BASE_DIR} ftp://ftp.ncbi.nlm.nih.gov/blast/executables/rmblast/LATEST/ncbi-rmblastn-2.2.28-x64-linux.tar.gz
 
-if [ ! -e ${BASE_DIR}/RepeatMasker-open-4-0-7.tar.gz ];
-then
+if [ ! -e ${BASE_DIR}/RepeatMasker-open-4-0-7.tar.gz ]; then
     wget -N -P ${BASE_DIR} http://www.repeatmasker.org/RepeatMasker-open-4-0-7.tar.gz
 fi
 
