d4aa93aaadd11e42c00c614cf685f0059a266e53
wang-q <wang-q@outlook.com>
2018-03-26 05:36:24 +0800

    Don't install some modules

--- prepare/4-cpanm.sh
+++ prepare/4-cpanm.sh
@@ -7,8 +7,8 @@ NO_TEST=--notest
 #NO_TEST=
 
 # very basic
-cpanm --mirror-only --mirror $CPAN_MIRROR $NO_TEST Archive::Extract Config::Tiny DB_File File::Find::Rule Getopt::Long::Descriptive JSON::XS Text::CSV_XS YAML YAML::Syck
-cpanm --mirror-only --mirror $CPAN_MIRROR --notest AnyEvent App::Ack App::Cmd DBI EV MCE Moo Moose Perl::Tidy POE Template WWW::Mechanize
+cpanm --mirror-only --mirror $CPAN_MIRROR $NO_TEST Archive::Extract Config::Tiny DB_File File::Find::Rule Getopt::Long::Descriptive JSON JSON::XS Text::CSV_XS YAML YAML::Syck
+cpanm --mirror-only --mirror $CPAN_MIRROR $NO_TEST App::Ack App::Cmd DBI MCE Moo Moose Perl::Tidy Template WWW::Mechanize XML::Parser
 
 # linuxbrew's pkg-config (node's dependency) override system pkg-config, and don't inherit system PKG_CONFIG_PATH
 # See also https://github.com/Homebrew/linuxbrew/issues/47
@@ -30,7 +30,7 @@ cpanm --mirror-only --mirror $CPAN_MIRROR --notest Convert::Binary::C IO::Scalar
 
 cpanm --mirror-only --mirror $CPAN_MIRROR --notest Bio::Perl # BioPerl
 cpanm --mirror-only --mirror $CPAN_MIRROR $NO_TEST Bio::ASN1::EntrezGene Bio::DB::EUtilities Bio::Graphics
-cpanm --mirror-only --mirror $CPAN_MIRROR --notest Bio::Tools::Run::Alignment::Clustalw # BioPerl-Run
+cpanm --mirror-only --mirror $CPAN_MIRROR --notest CJFIELDS/BioPerl-Run-1.007002.tar.gz # BioPerl-Run
 
 # circos
 cpanm --mirror-only --mirror $CPAN_MIRROR $NO_TEST Config::General Data::Dumper Digest::MD5 Font::TTF::Font Math::Bezier Math::BigFloat Math::Round Math::VecStat
@@ -38,7 +38,7 @@ cpanm --mirror-only --mirror $CPAN_MIRROR $NO_TEST Params::Validate Readonly Reg
 
 # Bio::Phylo
 cpanm --mirror-only --mirror $CPAN_MIRROR $NO_TEST XML::XML2JSON PDF::API2 Math::CDF Math::Random
-cpanm --mirror-only --mirror $CPAN_MIRROR --notest Bio::Phylo
+cpanm --mirror-only --mirror $CPAN_MIRROR $NO_TEST Bio::Phylo
 
 # others
 cpanm --mirror-only --mirror $CPAN_MIRROR $NO_TEST DateTime::Format::Natural DBD::CSV Path::Class File::HomeDir File::Listing File::Remove File::Copy::Recursive String::Compare
@@ -53,5 +53,5 @@ cpanm --mirror-only --mirror $CPAN_MIRROR --notest Excel::Writer::XLSX Spreadshe
 
 # AlignDB::*
 cpanm --mirror-only --mirror $CPAN_MIRROR $NO_TEST AlignDB::IntSpan AlignDB::IntSpanXS AlignDB::Stopwatch
-cpanm --mirror-only --mirror $CPAN_MIRROR $NO_TEST AlignDB::Codon AlignDB::DeltaG AlignDB::GC AlignDB::SQL AlignDB::Window AlignDB::ToXLSX AlignDB::Run
+cpanm --mirror-only --mirror $CPAN_MIRROR $NO_TEST AlignDB::Codon AlignDB::DeltaG AlignDB::GC AlignDB::SQL AlignDB::Window AlignDB::ToXLSX
 cpanm --mirror-only --mirror $CPAN_MIRROR $NO_TEST App::Fasops App::RL App::Rangeops
