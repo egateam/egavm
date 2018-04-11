b09a54d4247a841e005090c34089aa3ac9507bb6
wang-q <wang-q@outlook.com>
2018-04-11 15:19:16 +0800

    Download more

--- prepare/resource/download.sh
+++ prepare/resource/download.sh
@@ -23,11 +23,23 @@ wget -N -P ${BASE_DIR} https://linuxbrew.bintray.com/bottles/blast-2.7.1.x86_64_
 wget -N -P ${BASE_DIR} https://linuxbrew.bintray.com/bottles/r-3.4.4.x86_64_linux.bottle.tar.gz
 wget -N -P ${BASE_DIR} https://linuxbrew.bintray.com/bottles/mysql-5.7.21.x86_64_linux.bottle.tar.gz
 
-# 7-brew.sh bioinformatics tools
+# bioinformatics tools
 wget -N -P ${BASE_DIR} ftp://ftp.ncbi.nlm.nih.gov/blast/executables/rmblast/LATEST/ncbi-rmblastn-2.2.28-x64-linux.tar.gz
 
 if [ ! -e ${BASE_DIR}/RepeatMasker-open-4-0-7.tar.gz ]; then
     wget -N -P ${BASE_DIR} http://www.repeatmasker.org/RepeatMasker-open-4-0-7.tar.gz
+fi
+
+if [ ! -e ${BASE_DIR}/repeatmaskerlibraries-20140131.tar.gz ]; then
+    wget -N -P ${BASE_DIR} https://github.com/egateam/egavm/releases/download/20170907/repeatmaskerlibraries-20140131.tar.gz
+fi
+
+if [ ! -e ${BASE_DIR}/jkbin-darwin-2011.tar.gz ]; then
+    wget -N -P ${BASE_DIR} https://github.com/egateam/egavm/releases/download/20170907/jkbin-darwin-2011.tar.gz
+fi
+
+if [ ! -e ${BASE_DIR}/jkbin-ubuntu-1404-2011.tar.gz ]; then
+    wget -N -P ${BASE_DIR} https://github.com/egateam/egavm/releases/download/20170907/jkbin-ubuntu-1404-2011.tar.gz
 fi
 
 # extra/7-mysql.sh
