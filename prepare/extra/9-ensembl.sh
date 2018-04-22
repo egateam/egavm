1c51885bcd6351a5db97c9fd6e6f72742f33a970
wang-q <wang-q@outlook.com>
2018-04-22 20:28:31 +0800

    Update prepare/extra/9-ensembl.sh

--- prepare/extra/9-ensembl.sh
+++ prepare/extra/9-ensembl.sh
@@ -4,25 +4,29 @@ export ENSEMBL_VERSION='82'
 
 cd /prepare/resource
 
-if [ ! -e /prepare/resource/ensembl-${ENSEMBL_VERSION}.tar.gz ];
-then
+if [ ! -e /prepare/resource/ensembl-${ENSEMBL_VERSION}.tar.gz ]; then
     echo "==> Get ensembl tarballs"
     wget https://github.com/Ensembl/ensembl/archive/release/${ENSEMBL_VERSION}.tar.gz           -O ensembl-${ENSEMBL_VERSION}.tar.gz
     wget https://github.com/Ensembl/ensembl-compara/archive/release/${ENSEMBL_VERSION}.tar.gz   -O ensembl-compara-${ENSEMBL_VERSION}.tar.gz
     wget https://github.com/Ensembl/ensembl-variation/archive/release/${ENSEMBL_VERSION}.tar.gz -O ensembl-variation-${ENSEMBL_VERSION}.tar.gz
 fi
 
+SITE_PERL=$(perl -e '($first) = grep {/site_perl/} grep {!/darwin/i and !/x86_64/} @INC; print $first')
+
 echo "==> ensembl"
-tar xfz /prepare/resource/ensembl-${ENSEMBL_VERSION}.tar.gz ensembl-release-${ENSEMBL_VERSION}/modules/Bio/EnsEMBL
-cp -R ensembl-release-${ENSEMBL_VERSION}/modules/Bio `perl -e 'print grep {/site_perl/ and /plenv/} grep {!/darwin/i and !/x86_64/} @INC'`
+tar xfz /prepare/resource/ensembl-${ENSEMBL_VERSION}.tar.gz \
+    ensembl-release-${ENSEMBL_VERSION}/modules/Bio/EnsEMBL
+cp -R ensembl-release-${ENSEMBL_VERSION}/modules/Bio ${SITE_PERL}
 
 echo "==> ensembl-compara"
-tar xfz /prepare/resource/ensembl-compara-${ENSEMBL_VERSION}.tar.gz ensembl-compara-release-${ENSEMBL_VERSION}/modules/Bio/EnsEMBL
-cp -R ensembl-compara-release-${ENSEMBL_VERSION}/modules/Bio `perl -e 'print grep {/site_perl/ and /plenv/} grep {!/darwin/i and !/x86_64/} @INC'`
+tar xfz /prepare/resource/ensembl-compara-${ENSEMBL_VERSION}.tar.gz \
+    ensembl-compara-release-${ENSEMBL_VERSION}/modules/Bio/EnsEMBL
+cp -R ensembl-compara-release-${ENSEMBL_VERSION}/modules/Bio ${SITE_PERL}
 
 echo "==> ensembl-variation"
-tar xfz /prepare/resource/ensembl-variation-${ENSEMBL_VERSION}.tar.gz ensembl-variation-release-${ENSEMBL_VERSION}/modules/Bio/EnsEMBL
-cp -R ensembl-variation-release-${ENSEMBL_VERSION}/modules/Bio `perl -e 'print grep {/site_perl/ and /plenv/} grep {!/darwin/i and !/x86_64/} @INC'`
+tar xfz /prepare/resource/ensembl-variation-${ENSEMBL_VERSION}.tar.gz \
+    ensembl-variation-release-${ENSEMBL_VERSION}/modules/Bio/EnsEMBL
+cp -R ensembl-variation-release-${ENSEMBL_VERSION}/modules/Bio ${SITE_PERL}
 
 rm -fr ensembl-release-${ENSEMBL_VERSION}
 rm -fr ensembl-compara-release-${ENSEMBL_VERSION}
